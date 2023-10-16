4b4846f8bf044af959b3d59389a1c655ad44d4d3
Lim SeongHyeon <98510182+DataHyeon@users.noreply.github.com>
2023-10-16 17:46:38 +0900

    [level 2] Title: 주식가격, Time: 93.09 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,10 @@
 def solution(prices):
-    answer = [0] * len(prices)  # 결과값을 저장할 리스트 초기화
-    stack = []  # 시점의 인덱스를 저장할 스택
-
-    for i, price in enumerate(prices):
-        # 스택이 비어있지 않고, 스택의 최상단에 있는 가격이 현재 가격보다 클 때
-        while stack and prices[stack[-1]] > price:
-            j = stack.pop()  # 인덱스 pop
-            answer[j] = i - j  # 결과값 저장
-        stack.append(i)  # 현재 시점의 인덱스 push
-
-    # 스택에 남아있는 인덱스 처리
-    while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - j - 1
-
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
     return answer
