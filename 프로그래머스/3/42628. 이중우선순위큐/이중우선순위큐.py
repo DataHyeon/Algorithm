fee4ae634a02837db3d2358a0b0b553b63c2acb4
Lim SeongHyeon <98510182+DataHyeon@users.noreply.github.com>
2023-10-17 11:50:44 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,36 +1,47 @@
 import heapq
 def solution(operations):
-    answer = []
     
     heap = []
-    now = None
     
-    while operations:
-        for operation in operations:
+    for operation in operations:
+        
+        # 추가하는 부분            
+        if "I" in operation:
+            insert_num = int(operation[2:])
             
-            if "I" in operation:
-                insert_num = int(operation[2:])
-                heapq.heappush(heap, insert_num)
+            # 힙 푸시할 경우 자동으로 최소힙 특성 적용
+            heapq.heappush(heap, insert_num)
+        
+        # 삭제하는 부분
+        elif "D" in operation:
             
-            elif "D" in operation:
+            # 최소힙으로 변환 후 아래 연산
+            heapq.heapify(heap)
+            
+            # 힙이 비어 있으면 operations에서 원소 검사 후 재연산
+            if not heap:
+                continue
+            
+            # 최댓값 삭제
+            elif "-" not in operation:
+                
+                # 모든 원소에 - 기호를 붙이고 최소힙으로 재변환
+                heap = [-x for x in heap]
                 heapq.heapify(heap)
                 
-                if not heap:
-                    continue
+                # 최댓값 삭제 이후 - 기호를 다시 붙이고 최소힙으로 재변환
+                heapq.heappop(heap)
+                heap = [-x for x in heap]
+            
+            # 최솟값 삭제
+            else:
+                heapq.heappop(heap)
     
-                elif "-" not in operation:
-                    heap = [-x for x in heap]
-                    heapq.heapify(heap)
-                    heapq.heappop(heap)
-                    heap = [-x for x in heap]
+    # 힙이 비어있는 경우
+    if len(heap) == 0:
+        return [0,0]
+    
+    # 아닌 경우
+    else:
+        return [max(heap), min(heap)]
 
-                else:
-                    heapq.heappop(heap)
-        
-        if len(heap) == 0:
-            return [0,0]
-        
-        else:
-            return [max(heap), min(heap)]
-        
-        
