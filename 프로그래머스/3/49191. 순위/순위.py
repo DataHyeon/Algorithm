30d5741f28ee93ff13c275d1f415789051557436
Lim SeongHyeon <98510182+DataHyeon@users.noreply.github.com>
2023-10-19 19:17:48 +0900

    [level 3] Title: 순위, Time: 5.50 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -16,13 +16,11 @@ def solution(n, results):
         for loser in win[i]:
             # i선수에게 진 선수의 패배 목록을 i선수에게 이긴 사람들의 목록으로 업데이트
             lose[loser].update(lose[i])
+            
         # i선수를 이긴사람
         for winner in lose[i]:
             # i선수를 이긴 선수의 승리 목록을 i선수를 이긴 선수의 승리 목록을 i선수의 승리 목록으로 업데이트
             win[winner].update(win[i])
             
-    print(win)
-    print(lose)
-    
-    # 이긴 사람의 수와 진 사람의 수의 합이 n-1인 선수의 수를 세서 반환합니다.
+    # 이긴 사람의 수와 진 사람의 수의 합이 n-1인 선수의 수를 세서 반환
     return len([1 for i in range(1, n+1) if len(win[i]) + len(lose[i]) == n-1])
